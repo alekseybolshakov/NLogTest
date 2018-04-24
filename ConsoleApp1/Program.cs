9e3cf45abb30eeb1ae8fa90d17150a9129096547
Алексей Большаков <lexeybad@yandex.ru>
2018-04-24 15:47:32 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        static void Main()
+        {
+            logger.Info("Начинаем запуск приложения");
+            try
+            {
+                throw new Exception("Упс");
+            }
+            catch (Exception exeption)
+            {
+                logger.Error(exeption, "Ошибка запуска приложения");
+            }
+            LogManager.Flush();
+        }
+    }
+}
